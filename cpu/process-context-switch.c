3d7bade794cbea9d38982495e14a5e826d826cfb
qsdrqs <qsdrqs@gmail.com>
2022-10-17 03:26:11 -0700

    move measurement codes to macro functions

--- cpu/process-context-switch.c
+++ cpu/process-context-switch.c
@@ -12,6 +12,24 @@
 
 #define N 1000000
 
+#define MEASURE_START()                                                  \
+    asm volatile(                                                        \
+        "cpuid\n\t"                                                      \
+        "rdtsc\n\t"                                                      \
+        "mov %%edx, %0\n\t"                                              \
+        "mov %%eax, %1\n\t"                                              \
+        : "=r"(cycles_high0), "=r"(cycles_low0)::"%rax", "%rbx", "%rcx", \
+          "%rdx")
+
+#define MEASURE_END()                                                    \
+    asm volatile(                                                        \
+        "rdtscp\n\t"                                                     \
+        "mov %%edx, %0\n\t"                                              \
+        "mov %%eax, %1\n\t"                                              \
+        "cpuid\n\t"                                                      \
+        : "=r"(cycles_high1), "=r"(cycles_low1)::"%rax", "%rbx", "%rcx", \
+          "%rdx")
+
 int main(int argc, char *argv[]) {
     uint32_t cycles_low0;
     uint32_t cycles_high0;
@@ -19,8 +37,8 @@ int main(int argc, char *argv[]) {
     uint32_t cycles_high1;
 
     // pipe
-    int pipefd[2]; // pipefd[0] is for reading, pipefd[1] is for writing
-    int pipefd2[2]; // pipefd2[0] is for reading, pipefd2[1] is for writing
+    int pipefd[2];   // pipefd[0] is for reading, pipefd[1] is for writing
+    int pipefd2[2];  // pipefd2[0] is for reading, pipefd2[1] is for writing
     if (pipe(pipefd) != 0) {
         perror("pipe creation failed");
         return 1;
@@ -64,22 +82,10 @@ int main(int argc, char *argv[]) {
         }
         uint64_t rw_sum = 0;
         for (int i = 0; i < N; ++i) {
-            asm volatile(
-                "cpuid\n\t"
-                "rdtsc\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                : "=r"(cycles_high0), "=r"(cycles_low0)::"%rax", "%rbx", "%rcx",
-                  "%rdx");
+            MEASURE_START();
             write(measure_rw[1], random_buf, sizeof(random_buf));
             read(measure_rw[0], random_buf, sizeof(random_buf));
-            asm volatile(
-                "rdtscp\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                "cpuid\n\t"
-                : "=r"(cycles_high1), "=r"(cycles_low1)::"%rax", "%rbx", "%rcx",
-                  "%rdx");
+            MEASURE_END();
             rw_sum += (((uint64_t)cycles_high1 << 32) | cycles_low1) -
                       (((uint64_t)cycles_high0 << 32) | cycles_low0);
         }
@@ -89,31 +95,19 @@ int main(int argc, char *argv[]) {
         // measure 2 time of context switching plus read and write of pipe
         uint64_t srw2_sum = 0;
         for (int i = 0; i < N; ++i) {
-            asm volatile(
-                "cpuid\n\t"
-                "rdtsc\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                : "=r"(cycles_high0), "=r"(cycles_low0)::"%rax", "%rbx", "%rcx",
-                  "%rdx");
+            MEASURE_START();
             write(pipefd[1], random_buf, sizeof(random_buf));
             read(pipefd2[0], random_buf, sizeof(random_buf));
-            asm volatile(
-                "rdtscp\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                "cpuid\n\t"
-                : "=r"(cycles_high1), "=r"(cycles_low1)::"%rax", "%rbx", "%rcx",
-                  "%rdx");
+            MEASURE_END();
             srw2_sum += (((uint64_t)cycles_high1 << 32) | cycles_low1) -
                         (((uint64_t)cycles_high0 << 32) | cycles_low0);
         }
         printf(
             "average time of 2 times of context switching plus read and write "
-            "of pipe: "
-            "%lu cycles\n",
+            "of pipe: %lu cycles\n",
             srw2_sum / N);
-        uint64_t context_switching_time = (srw2_sum / N - 2 * rw_sum / N + 27) / 2;
+        uint64_t context_switching_time =
+            (srw2_sum / N - 2 * rw_sum / N + 27) / 2;
         printf("average time of context switching: %lu cycles\n",
                context_switching_time);
         close(pipefd[1]);
--- cpu/thread-context-switch.c
+++ cpu/thread-context-switch.c
@@ -12,6 +12,24 @@
 
 #define N 1000000
 
+#define MEASURE_START()                                                  \
+    asm volatile(                                                        \
+        "cpuid\n\t"                                                      \
+        "rdtsc\n\t"                                                      \
+        "mov %%edx, %0\n\t"                                              \
+        "mov %%eax, %1\n\t"                                              \
+        : "=r"(cycles_high0), "=r"(cycles_low0)::"%rax", "%rbx", "%rcx", \
+          "%rdx")
+
+#define MEASURE_END()                                                    \
+    asm volatile(                                                        \
+        "rdtscp\n\t"                                                     \
+        "mov %%edx, %0\n\t"                                              \
+        "mov %%eax, %1\n\t"                                              \
+        "cpuid\n\t"                                                      \
+        : "=r"(cycles_high1), "=r"(cycles_low1)::"%rax", "%rbx", "%rcx", \
+          "%rdx")
+
 int pipefd[2];   // pipefd[0] is for reading, pipefd[1] is for writing
 int pipefd2[2];  // pipefd2[0] is for reading, pipefd2[1] is for writing
 
@@ -26,29 +44,18 @@ void *context_switch(void *thread_id) {
         int random_buf[1] = {0};
 
         // measure the time of read and write of pipe
-        int measure_rw[2];  // for measuring read/write cpu clocks, measure_rw[0] is for
-                            // reading, measure_rw[1] is for writing
+        int measure_rw[2];  // for measuring read/write cpu clocks,
+                            // measure_rw[0] is for reading, measure_rw[1] is
+                            // for writing
         if (pipe(measure_rw) != 0) {
             perror("pipe creation failed");
         }
         uint64_t rw_sum = 0;
         for (int i = 0; i < N; ++i) {
-            asm volatile(
-                "cpuid\n\t"
-                "rdtsc\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                : "=r"(cycles_high0), "=r"(cycles_low0)::"%rax", "%rbx", "%rcx",
-                  "%rdx");
+            MEASURE_START();
             write(measure_rw[1], random_buf, sizeof(random_buf));
             read(measure_rw[0], random_buf, sizeof(random_buf));
-            asm volatile(
-                "rdtscp\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                "cpuid\n\t"
-                : "=r"(cycles_high1), "=r"(cycles_low1)::"%rax", "%rbx", "%rcx",
-                  "%rdx");
+            MEASURE_END();
             rw_sum += (((uint64_t)cycles_high1 << 32) | cycles_low1) -
                       (((uint64_t)cycles_high0 << 32) | cycles_low0);
         }
@@ -58,31 +65,19 @@ void *context_switch(void *thread_id) {
         // measure 2 time of context switching plus read and write of pipe
         uint64_t srw2_sum = 0;
         for (int i = 0; i < N; ++i) {
-            asm volatile(
-                "cpuid\n\t"
-                "rdtsc\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                : "=r"(cycles_high0), "=r"(cycles_low0)::"%rax", "%rbx", "%rcx",
-                  "%rdx");
+            MEASURE_START();
             write(pipefd[1], random_buf, sizeof(random_buf));
             read(pipefd2[0], random_buf, sizeof(random_buf));
-            asm volatile(
-                "rdtscp\n\t"
-                "mov %%edx, %0\n\t"
-                "mov %%eax, %1\n\t"
-                "cpuid\n\t"
-                : "=r"(cycles_high1), "=r"(cycles_low1)::"%rax", "%rbx", "%rcx",
-                  "%rdx");
+            MEASURE_END();
             srw2_sum += (((uint64_t)cycles_high1 << 32) | cycles_low1) -
                         (((uint64_t)cycles_high0 << 32) | cycles_low0);
         }
         printf(
             "average time of 2 times of context switching plus read and write "
-            "of pipe: "
-            "%lu cycles\n",
+            "of pipe: %lu cycles\n",
             srw2_sum / N);
-        uint64_t context_switching_time = (srw2_sum / N - 2 * rw_sum / N + 27) / 2;
+        uint64_t context_switching_time =
+            (srw2_sum / N - 2 * rw_sum / N + 27) / 2;
         printf("average time of context switching: %lu cycles\n",
                context_switching_time);
         close(pipefd[1]);

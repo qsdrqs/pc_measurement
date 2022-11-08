8b774f43eea494cec1f692a708f6cad688e81e6e
AnzeXie <a1xie@ucsd.edu>
2022-11-08 00:25:32 -0800

    Fixed access_latency measurement

--- memory/access_latency.c
+++ memory/access_latency.c
@@ -24,18 +24,21 @@
 		  : "=r" (cycles_high1), "=r" (cycles_low1)  \
 		  :: "%rax", "%rbx", "%rcx", "%rdx")
 
-#define NUM_RUN 5  // number of runs for each experiment
+#define NUM_RUN 8  // number of runs for each experiment
 #define STRIDE_SIZE_BASE 2 
 #define ARRAY_SIZE_BASE 2
 #define NUM_LOAD 1000000
-#define MAX_ARRAY_SIZE_LOG 30
+#define MAX_ARRAY_SIZE_POWER 30
 
-int32_t stride_size;
-int32_t num_int; // number of integers in array
-int32_t current_access_num; // number that is being accessing
-int32_t array_size;
+int64_t stride_size;
+int64_t stride_num; // stride_size / sizeof(int*)
+int64_t num_int; // number of integers in array
+int** current_access_ptr; // ptr that is being accessing
+int64_t array_size;
+int64_t num_ptr;
 
 int64_t total_time = 0;
+int64_t loop_time = 0;
 u_int32_t cycles_high0;
 u_int32_t cycles_low0;
 u_int32_t cycles_high1;
@@ -48,44 +51,57 @@ int main(int argc, char* argv[]){
     fflush(stdout);
     results_file = fopen("access_latency_results.txt", "a");
 
-
-    // load integer array size from 512 bytes to  MBs. (128 -  integers)
-    
-
-    for (int i = 9; i <= MAX_ARRAY_SIZE_LOG; i++ ){ // max array size is  MB, min is 512B
+    for (int i = 9; i <= MAX_ARRAY_SIZE_POWER; i++){
         array_size = pow(ARRAY_SIZE_BASE, i);
-        num_int = array_size / sizeof(int32_t); 
-        int* array = (int*)calloc(num_int, sizeof(int32_t));
+        num_ptr = array_size / sizeof(int*);
+        int** array = (int**)malloc(sizeof(int*) * num_ptr);
 
-        for (int j = 6; j < 13; j++){ // use 6 different stride sizes, min is 64B, max is 4KB
-            stride_size = pow(STRIDE_SIZE_BASE, j); // unit is int size
-            printf("running [2^(%i), %i]Bytes\n", i, stride_size);
+        for (int j = 6; j < 13; j++){
+            stride_size = pow(STRIDE_SIZE_BASE, j);
+            stride_num = stride_size / sizeof(int*);
+
+            printf("Preparing [array_size, num_ptr, stride_size] = [2^(%i), %li, %i]\n", i, num_ptr, stride_size);
             fflush(stdout);
-
-            for (int l = 0; l < NUM_RUN; l++){ // run a few times and take the last result
-                total_time = 0;
-                for (int k = 0; k < NUM_LOAD; k++){
-                    START_MEASUREMENT();
-
-                    current_access_num = array[(k * stride_size / sizeof(int32_t)) % num_int]; // load instruction
-                    // printf("current_access_num=%i, pos=%i  ", current_access_num, (k * stride_size) % array_size);
-
-                    END_MEASUREMENT();
-
-                    total_time = total_time + (((int64_t)cycles_high1 << 32) | cycles_low1) - (((int64_t)cycles_high0 << 32) | cycles_low0);
-                }
+            for (int k = 0; k < (array_size / stride_size); k++){
+                array[(k * stride_num) % num_ptr] = (int*)&array[(k * stride_num + stride_num) % num_ptr];
+                // printf("[%p], [%p], [%li, %li]\n", 
+                //                             &array[(k * stride_num) % num_ptr], // element addr
+                //                             array[(k * stride_num) % num_ptr],  // addr pointing to
+                //                             (k * stride_num)%num_ptr,
+                //                             (k * stride_num));
             }
 
+            printf("Walking [array_size, num_ptr, stride_size] = [2^(%i), %li, %i]\n", i, num_ptr, stride_size);
+            fflush(stdout);
+            
+            for (int l = 0; l < NUM_RUN; l++){
+                total_time = 0;
+                current_access_ptr = array[0];
+                // printf("**%p**\n", current_access_ptr);
+                START_MEASUREMENT();
+                for (int k = 0; k < NUM_LOAD; k++){
+                    current_access_ptr = *current_access_ptr;
+                    // printf("**%p**\n", current_access_ptr);
+                }
+                END_MEASUREMENT();
+                total_time = (((int64_t)cycles_high1 << 32) | cycles_low1) - (((int64_t)cycles_high0 << 32) | cycles_low0);
+            }
+
+            loop_time = 0;
+            START_MEASUREMENT();
+            for (int k = 0; k < NUM_LOAD; k++){}
+            END_MEASUREMENT();
+
+            total_time = total_time - loop_time;
             if (NULL == results_file){
                 perror("Error opening result saving file.");
             }
             else{
                 fprintf(results_file, "(%i,%i,%lf)", i, stride_size, ((double)total_time / NUM_LOAD));
             }
-
         }
 
-        printf("array_size %i / %i completed\n\n", i, MAX_ARRAY_SIZE_LOG);
+        printf("array_size %i / %i completed\n\n", i, MAX_ARRAY_SIZE_POWER);
         if (NULL == results_file){
             perror("Error opening result saving file.");
         }
@@ -93,8 +109,54 @@ int main(int argc, char* argv[]){
             fprintf(results_file, "\n");
         }
 
-        free(array);
     }
 
     return(0);
+    
+    
+    
+    // load integer array size from 512 bytes to  MBs. (128 -  integers)
+    
+    
+    // for (int i = 9; i <= MAX_ARRAY_SIZE_LOG; i++ ){ // max array size is  MB, min is 512B
+    //     array_size = pow(ARRAY_SIZE_BASE, i);
+    //     num_int = array_size / sizeof(int32_t); 
+    //     int* array = (int*)calloc(num_int, sizeof(int32_t));
+
+    //     for (int j = 6; j < 8; j++){ // use 6 different stride sizes, min is 64B, max is 4KB
+    //         stride_size = pow(STRIDE_SIZE_BASE, j); // unit is int size
+    //         printf("running [2^(%i), %i]Bytes\n", i, stride_size);
+    //         fflush(stdout);
+
+    //         for (int l = 0; l < NUM_RUN; l++){ // run a few times and take the last result
+    //             total_time = 0;
+    //             START_MEASUREMENT();
+    //             for (int k = 0; k < NUM_LOAD; k++){
+    //                 current_access_num = array[(k * stride_size / sizeof(int32_t)) % num_int]; // load instruction
+    //                 // printf("current_access_num=%i, pos=%i  ", current_access_num, (k * stride_size) % array_size);
+    //             }
+    //             END_MEASUREMENT();
+    //             total_time = (((int64_t)cycles_high1 << 32) | cycles_low1) - (((int64_t)cycles_high0 << 32) | cycles_low0);
+    //         }
+
+    //         if (NULL == results_file){
+    //             perror("Error opening result saving file.");
+    //         }
+    //         else{
+    //             fprintf(results_file, "(%i,%i,%lf)", i, stride_size, ((double)total_time / NUM_LOAD));
+    //         }
+
+    //     }
+
+    //     printf("array_size %i / %i completed\n\n", i, MAX_ARRAY_SIZE_LOG);
+    //     if (NULL == results_file){
+    //         perror("Error opening result saving file.");
+    //     }
+    //     else{
+    //         fprintf(results_file, "\n");
+    //     }
+
+    //     //free(array);
+    // }
+
 }
--- memory/access_memory_data_postprocess.py
+++ memory/access_memory_data_postprocess.py
@@ -2,17 +2,32 @@ import numpy
 import statistics
 import re
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 def main():
     results = defaultdict(list)
 
-    f = open("./access_latency_results.txt", "r")
+    f = open("./memory/access_latency_results.txt", "r")
     for l in f.readlines():
         temp = re.findall("\d*,\d*,\d*\.\d*", l)
         for instance in temp:
             data = instance.split(',')
-            results[data[1]].append([data[0], data[2]]) # [array size, latency]
-        
+            results[data[1]].append([float(data[0]), float(data[2])]) # [array size, latency]
+    
+    for r in results:
+        x = []
+        y = []
+        for d in results[r]:
+            x.append(d[0])
+            y.append(d[1] * 0.27)
+        plt.plot(x,y, label=r)
+    plt.legend(loc='upper left')
+    plt.title("Access Latency")
+    plt.xlabel('log2(Array size)')
+    plt.ylabel('Latency (ns)')
+    plt.savefig("./access_latency.png")
+    pass
+
     
 
 if __name__ == "__main__":
